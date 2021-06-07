814b50933fdb733a55bbb27f4b54ba349f47cf52
Kenny White <kennywhite1@gmail.com>
2021-06-07 18:19:05 +0200

    Added terraform required_providers block

--- IaC/provider.tf
+++ IaC/provider.tf
@@ -1,6 +1,17 @@
-# Specify the provider and access details
+# Lock in the specify the provider version
+terraform {
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+# Set details for provider with local name
 provider "aws" {
   region = var.aws_region
+
   default_tags {
     tags = {
       Name    = "terraform"
