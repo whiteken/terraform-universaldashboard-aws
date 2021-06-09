3b62530fcc7ff7af752bc2ee18a12e52f5547722
Kenny White <kennywhite1@gmail.com>
2021-06-09 10:50:06 +0200

    locking down terraform required_version to 0.14.5

--- IaC/provider.tf
+++ IaC/provider.tf
@@ -1,5 +1,10 @@
 # Lock in the specify the provider version
 terraform {
+
+  #version of the terraform executable
+  required_version = "0.14.5"
+
+  #version of the aws provider
   required_providers {
     aws = {
       version = ">= 2.7.0"
